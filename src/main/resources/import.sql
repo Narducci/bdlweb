2c32a559b126147087b64180bb1ea3c71af33042
Narducci <orazio.narducci@gmail.com>
2023-08-24 15:32:04 -0300

    Modelo de domínio fase 1 concluído

---
+++ src/main/resources/import.sql
@@ -0,0 +1,14 @@
+INSERT INTO tb_category(name) VALUES ('REMUNERAÇÕES');
+INSERT INTO tb_category(name) VALUES ('DEPRECIAÇÕES');
+INSERT INTO tb_category(name) VALUES ('FORÇA MOTRIZ');
+INSERT INTO tb_category(name) VALUES ('DGF');
+
+INSERT INTO tb_category(name, category_id) VALUES ('SALÁRIOS', 1);
+INSERT INTO tb_category(name, category_id) VALUES ('ENCARGOS', 1);
+INSERT INTO tb_category(name, category_id) VALUES ('MATERIAIS DE OPERAÇÃO', 4);
+INSERT INTO tb_category(name, category_id) VALUES ('COMBUSTÍVEIS PARA PROCESSO', 4);
+INSERT INTO tb_category(name, category_id) VALUES ('FERRAMENTAS PERECÍVEIS', 4);
+INSERT INTO tb_category(name, category_id) VALUES ('UTILIDADES (LUZ/TELEFONE/ÁGUA)', 4);
+INSERT INTO tb_category(name, category_id) VALUES ('MANUTENÇÃO', 4);
+INSERT INTO tb_category(name, category_id) VALUES ('ALUGUÉIS, SEGUROS E TAXAS', 4);
+INSERT INTO tb_category(name, category_id) VALUES ('DESPESAS DIVERSAS', 4);
